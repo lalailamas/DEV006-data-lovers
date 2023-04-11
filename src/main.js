2092e515cc0754bf863594eb7c4ab5b73bbb483d
lalailamas <ale.cordoba.p@gmail.com>
2023-04-11 15:22:25 -0400

    filter characters logrado

--- src/main.js
+++ src/main.js
@@ -5,21 +5,66 @@ const characterInfo = document.getElementById('charactersPage');
 
 data.characters.forEach((item) => {
   const div = document.createElement('div');
-  div.innerHTML = `Name: ${item.name} <br>  Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+  div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
   characterInfo.appendChild(div);
 });
 
-/*const optionMale = document.getElementById("optionMale");
-data.characters.filter((gender) => {
- if (characters.gender === "Male") {
-     console.log (gender);
-  }
-  });*/
 
-document.getElementById("optionMale").innerHTML = data.characters.filter(gender);
-function gender (checkMale){
-  return checkMale;
+function maleFilter(character) {
+  if (character.gender === "Male") {
+    return true;
+  } else {
+    return false;
+  }
 }
+function femaleFilter(character) {
+  if (character.gender === "Female") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const selectGender = document.getElementById("selectGender");
+selectGender.addEventListener("change", (event) => {
+  console.log(selectGender.value);
+  /*const charactersFiltered = data.characters.filter(maleFilter);*/
+  
+  let charactersFiltered = [];
+  if (selectGender.value === "Female") {
+    charactersFiltered = data.characters.filter(femaleFilter);
+    const counterFemale = charactersFiltered.length
+    console.log(charactersFiltered.length);
+    document.getElementById("counter").innerHTML = "Characters females are  "+ counterFemale;
+  } else {
+    charactersFiltered = data.characters.filter (maleFilter);
+    const counterMale = charactersFiltered.length
+    document.getElementById("counter").innerHTML = "Characters males are  "+ counterMale;
+  }
+  console.log(charactersFiltered);
+  //limpiando el div que contiene la info de los personajes
+  document.getElementById("charactersPage").innerHTML = "";
+  
+  charactersFiltered.forEach((item) => {
+    const div = document.createElement('div');
+    div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+    characterInfo.appendChild(div);
+  });
+});
+
+
+
+
+
+
+/*data.characters.filter(function(filterGender, Gender));
+
+
+
+const optionMale = data.characters.filter(gender);
+function gender (optionMale){
+return true;*/
+
 
 
 

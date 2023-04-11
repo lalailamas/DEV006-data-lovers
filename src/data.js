8fe2c90698173ea0a6ff26c686898829ef7fa7d2
lalailamas <ale.cordoba.p@gmail.com>
2023-04-11 09:23:05 -0400

    trabajando en filter characters

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const data = () => {
+// return 'data';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+//return 'OMG';
+//};
 
 //aqui es donde van las funciones puras 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);
+import data from './data/harrypotter/data.js';
+
+const characterInfo = document.getElementById('charactersPage');
+
+data.characters.forEach((item) => {
+  const div = document.createElement('div');
+  div.innerHTML = `Name: ${item.name} <br>  Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+  characterInfo.appendChild(div);
+});
+
+/*const optionMale = document.getElementById("optionMale");
+data.characters.filter((gender) => {
+ if (characters.gender === "Male") {
+     console.log (gender);
+  }
+  });*/
+
+document.getElementById("optionMale").innerHTML = data.characters.filter(gender);
+function gender (checkMale){
+  return checkMale;
+}
 
 
+
+/*
+Para acortar data
+function myFunction(item, index, arr) {
+    arr[index] = item * 10;
+  }*/

e9f34e283008f61136d5febef80bddb9a14e539b
lalailamas <ale.cordoba.p@gmail.com>
2023-04-20 18:31:05 -0400

    test listos

--- src/data.js
+++ src/data.js
@@ -1,14 +1,7 @@
-// estas funciones son de ejemplo
-
-//export const data = () => {
-// return 'data';
-//};
-
-//export const anotherExample = () => {
-//return 'OMG';
-//};
 
 //aqui es donde van las funciones puras 
+
+
 
 // FUNCION FILTRADO
 export function maleFilter(character) {
@@ -24,4 +17,44 @@ export function femaleFilter(character={}) {
   } else {
     return false;
   }
+}
+
+// FUNCION QUE RECORRE DATA 
+export function formatCharacters(characters) {
+  return characters.map((item) => {
+    return {
+      name: item.name,
+      birth: item.birth,
+      gender: item.gender
+    }
+  });
+}
+
+//FUNCION ORDENAR
+export function sortCharacters(characters, orderByValue) {
+  if (orderByValue === "A-z") {
+    // ordenar AZ
+    return characters.sort((azSort, zaSort) => {
+      if (azSort.name < zaSort.name) {
+        return -1;
+      }
+      return 1;
+    });
+  } else {
+    // ordenar ZA
+    return characters.sort((azSort, zaSort) => {
+      if (azSort.name > zaSort.name) {
+        return -1;
+      }
+      return 1;
+    });
+  }
+}
+
+//FUNCION PARA INPUT BUSCAR
+export function filterByName(characters, searchText) {
+  return characters.filter((character) => {
+    //incluir letras mayusculas y minusculas
+    return character.name.toLowerCase().includes(searchText.toLowerCase());
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
 
 import data from './data/harrypotter/data.js';
-import { maleFilter, femaleFilter } from './data.js';
+import { maleFilter, femaleFilter, formatCharacters, sortCharacters, filterByName } from './data.js';
 
+//Manipulación del DOM
+
+//Mostrar la data
 const characterInfo = document.getElementById('charactersPage');
-
-// MOSTRAR LA DATA
-data.characters.forEach((item) => {
+const formattedCharacters = formatCharacters(data.characters);
+formattedCharacters.forEach((item) => {
   const div = document.createElement('div');
-  div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+  div.innerHTML =
+    `Name: ${item.name}
+   Birthday: ${item.birth} 
+   Gender: ${item.gender}`;
   characterInfo.appendChild(div);
 });
 
 
-//declaramos nueva variable de data
+//Declaramos nueva variable de data para unificar funciones
 let newData;
+
+//Función filtrado
 const selectGender = document.getElementById("selectGender");
 selectGender.addEventListener("change", () => {
   let charactersFiltered = [];
@@ -27,82 +34,68 @@ selectGender.addEventListener("change", () => {
     const counterMale = charactersFiltered.length
     document.getElementById("counter").innerHTML = "Characters males are  " + counterMale;
   }
- 
+
   newData = charactersFiltered;
 
-  //limpiando el div que contiene la info de los personajes
+  //Limpiando el div que contiene la info de los personajes
   document.getElementById("charactersPage").innerHTML = "";
   // Mostrar los personajes filtrados
   charactersFiltered.forEach((item) => {
     const div = document.createElement('div');
-    div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+    div.innerHTML =
+      `Name: ${item.name}
+    Birthday: ${item.birth}
+    Gender: ${item.gender}`;
     characterInfo.appendChild(div);
   });
 });
 
 /*---------------------------------------------------------------*/
-
+//Función de buscar
 // Controlador de eventos para el elemento de búsqueda
 const searchInput = document.getElementById("searchInput");
 searchInput.addEventListener("input", (event) => {
   const searchText = event.target.value;
-  searchCharacters(searchText);
+  searchCharactersAndShowResults(searchText, data.characters);
 });
 
-// FUNCION BUSCAR
-export function searchCharacters(searchText) {
-  //comparando el nuevo array filtrado (newData) con la data original 
-  const finishData = newData ? newData : data.characters;
-  const charactersFiltered = finishData.filter((character) => {
-    //incluir letras mayusculas y minusculas
-    return character.name.toLowerCase().includes(searchText.toLowerCase());
-  });
+function searchCharactersAndShowResults(searchText, characters) {
+  //Comparando el nuevo array filtrado (newData) con la data original 
+  const finishData = newData ? newData : characters;
+  const charactersFiltered = filterByName(finishData, searchText);
 
   // Limpiar el div que contiene la info de los personajes
   document.getElementById("charactersPage").innerHTML = "";
 
-
   // Mostrar los personajes filtrados
   charactersFiltered.forEach((item) => {
     const div = document.createElement("div");
-    div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
+    div.innerHTML =
+      `Name: ${item.name}
+    Birthday: ${item.birth}
+    Gender: ${item.gender}`;
     characterInfo.appendChild(div);
   });
 }
 
 /*-------------------------------------------------------------------*/
 
-//FUNCION DE ORDENAR 
+//Función de ordenar
 const selectSort = document.getElementById("selectSort");
 selectSort.addEventListener("change", () => {
-  //value aZ zA
   const orderByValue = event.target.value;
-  if (orderByValue === "A-z"){
-    //ordenar AZ
-    data.characters.sort((azSort, zaSort) => {
-      if (azSort.name < zaSort.name){
-        return -1;
-      } 
-      return 1;
-    });
-  } else {
-    //ordenar ZA
-    data.characters.sort((azSort, zaSort) => {
-      if (azSort.name > zaSort.name){
-        return -1;
-      } 
-      return 1;
-    });
-  }
-  
-  document.getElementById("charactersPage").innerHTML = "";
-  // Mostrar los personajes filtrados
-  data.characters.forEach((item) => {
-    const div = document.createElement("div");
-    div.innerHTML = `Name: ${item.name} <br> Birthday: ${item.birth} <br>  Gender: ${item.gender}`;
-    characterInfo.appendChild(div);
-  });
+  const sortedCharacters = sortCharacters(data.characters, orderByValue);
+  renderCharacters(sortedCharacters);
 });
 
-
-/*---------------------------------------------------------------*/
+function renderCharacters(characters) {
+  document.getElementById("charactersPage").innerHTML = "";
+  characters.forEach((item) => {
+    const div = document.createElement("div");
+    div.innerHTML =
+      `Name: ${item.name}
+    Birthday: ${item.birth}
+    Gender: ${item.gender}`;
+    characterInfo.appendChild(div);
+  });
+}
